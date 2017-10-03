ba685c708a041a0095e7a82107ae8253fa4b6ba5
Alex Carreiro <alexxcarreiroo@gmail.com>
2017-10-03 10:06:11 -0400

    Update sendmessage.js

--- sendmessage.js
+++ sendmessage.js
@@ -1,5 +1,5 @@
-var accountSid = 'ACc0d6f53e10172dcf5910ee5ca795cf51'; // Your Account SID from www.twilio.com/console
-var authToken = '9a158610c68fe25258ed661a2145bc7c';   // Your Auth Token from www.twilio.com/console
+var accountSid = ''; // Your Account SID from www.twilio.com/console
+var authToken = '';   // Your Auth Token from www.twilio.com/console
 
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
