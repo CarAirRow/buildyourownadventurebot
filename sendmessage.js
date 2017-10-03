2782aeada0b00b683180c8378ac77f253a103091
Alex Carreiro <alexxcarreiroo@gmail.com>
2017-10-03 10:06:34 -0400

    Update sendmessage.js

--- sendmessage.js
+++ sendmessage.js
@@ -6,7 +6,7 @@ var client = new twilio(accountSid, authToken);
 
 client.messages.create({
     body: 'Hello from Node #2',
-    to: '+12267551471',  // Text this number
-    from: '+12264002164 ' // From a valid Twilio number
+    to: '',  // Text this number
+    from: '' // From a valid Twilio number
 })
 .then((message) => console.log(message));
