7666b06448dd8edd3342adab8dcc6615d23db6c8
Alex Carreiro <Acarreiro-cc@conestogac.on.ca>
2017-10-03 10:02:39 -0400

    update

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.get("/api/chatbot", (req, res)=>{
     res.send("Hello from the chatbot");
 
 });
+// Define a method for twillio webhook
+app.post('/sms', function(req, res) {
+  var twilio = require('twilio');
+  var twiml = new twilio.TwimlResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
 
 // Listen for requests
 var server = app.listen(app.get('port'), () =>{
--- sendmessage.js
+++ sendmessage.js
@@ -1,12 +1,12 @@
 var accountSid = 'ACc0d6f53e10172dcf5910ee5ca795cf51'; // Your Account SID from www.twilio.com/console
-var authToken = 'your_auth_token';   // Your Auth Token from www.twilio.com/console
+var authToken = '9a158610c68fe25258ed661a2145bc7c';   // Your Auth Token from www.twilio.com/console
 
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 
 client.messages.create({
-    body: 'Hello from Node',
-    to: '+12345678901',  // Text this number
-    from: '+12345678901' // From a valid Twilio number
+    body: 'Hello from Node #2',
+    to: '+12267551471',  // Text this number
+    from: '+12264002164 ' // From a valid Twilio number
 })
-.then((message) => console.log(message.sid));
+.then((message) => console.log(message));
